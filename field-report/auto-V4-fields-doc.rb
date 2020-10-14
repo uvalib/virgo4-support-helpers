abc38daf01d87b72f8bfee522e18a50de1d92d74
Michael Durbin <md5wz@virginia.edu>
2020-10-14 14:48:32 -0400

    VIRGONEW-1747: minor changes to make script easier to invoke

--- field-report/auto-V4-fields-doc.rb
+++ field-report/auto-V4-fields-doc.rb
@@ -3,48 +3,54 @@ require 'net/http'
 require 'uri'
 require 'json'
 require 'csv'
-require 'toml'
+require 'bundler/inline'
 
-$fields_path=ARGV[0]
-$pools_path=ARGV[1]
-$active_toml_path=ARGV[2]
+gemfile do
+  source 'https://rubygems.org'
+  gem "toml", "~> 0.2.0"
+end
+
+$terraform_infrastructure_path=ARGV[0] || "../../terraform-infrastructure"
+$virgo4_pool_solr_ws_path=ARGV[1] || "../../virgo4-pool-solr-ws"
+$environment=ARGV[2] || "staging"
 
 # Read in field.json file
-file=File.read($fields_path+"fields.json")
+file=File.read($terraform_infrastructure_path+"/virgo4.lib.virginia.edu/ecs-tasks/"+$environment+"/pool-solr-ws/environment/common/fields.json")
 fields=JSON.parse(file)['global']['mappings']['definitions']['fields']
 
 # Read in pool json file
 def pool(pool_name)
-  pool_file=File.read($pools_path+pool_name+".json")
+  pool_file=File.read($terraform_infrastructure_path+"/virgo4.lib.virginia.edu/ecs-tasks/"+$environment+"/pool-solr-ws/environment/pools/"+pool_name+".json")
   JSON.parse(pool_file)["local"]["mappings"]["configured"]["field_names"]["detailed"]
 end
 
-toml_hash = TOML.load_file($active_toml_path+"active.en.toml")
+toml_hash = TOML.load_file($virgo4_pool_solr_ws_path+"/i18n/active.en.toml")
 #puts toml_hash['FieldAbbreviatedTitle']['other']
 
 CSV.open("V4-fields.csv","w",
     :write_headers => true,
-    :headers => ["Field", "Label", "solr field", "archival", "catalog","hathitrust", "images", "maps", "music-recordings", "music-scores", "serials", "sound-recordings", "thesis", "uva-library", "video"]
+    :headers => ["Field", "Label", "solr field", "archival", "catalog","hathitrust", "images", "maps", "music-recordings", "musical-scores", "serials", "sound-recordings", "thesis", "uva-library", "video"]
 ) do |csv|
-  (0 ..fields.count).each do |i|
-    xid = fields[i]['xid']
-    line =  [fields[i]['name'],
+  fields.each do |field|
+    xid = field['xid']
+    line =  [field['name'],
              xid ? toml_hash[xid]['other'] : "No Label",
-             fields[i]['field'],
-             pool('archival').include?(fields[i]['name']),
-             pool('catalog').include?(fields[i]['name']),
-             pool('hathitrust').include?(fields[i]['name']),
-             pool('images').include?(fields[i]['name']),
-             pool('maps').include?(fields[i]['name']),
-             pool('music-recordings').include?(fields[i]['name']),
-             pool('music-scores').include?(fields[i]['name']),
-             pool('serials').include?(fields[i]['name']),
-             pool('sound-recordings').include?(fields[i]['name']),
-             pool('thesis').include?(fields[i]['name']),
-             pool('uva-library').include?(fields[i]['name']),
-             pool('video').include?(fields[i]['name'])
+             field['field'],
+             pool('archival').include?(field['name']),
+             pool('catalog').include?(field['name']),
+             pool('hathitrust').include?(field['name']),
+             pool('images').include?(field['name']),
+             pool('maps').include?(field['name']),
+             pool('music-recordings').include?(field['name']),
+             pool('musical-scores').include?(field['name']),
+             pool('serials').include?(field['name']),
+             pool('sound-recordings').include?(field['name']),
+             pool('thesis').include?(field['name']),
+             pool('uva-library').include?(field['name']),
+             pool('video').include?(field['name'])
          ]
     csv << line
   end
+  puts("Wrote field summary to V4-fields.csv.")
 
 end
